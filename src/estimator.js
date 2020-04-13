8659d767fc7a10e9685d01466162533d62abdf62
Joseph Moshood <gladstargtschool6@gmail.com>
2020-04-13 20:19:32 +0100

    Update estimator.js
    challenge1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,33 @@
 const covid19ImpactEstimator = (data) => data;
 
 export default covid19ImpactEstimator;
+
+function calculator(){
+  
+        var reportedCases = Number(document.getElementById("reportedCases").value);
+  
+        var days = Number(document.getElementById("days").value);
+  
+     var x = days / 3;
+  
+     var y = 2**x;
+  
+     var result;
+  
+  var calc = document.getElementById("choice").value
+  
+        switch(calc){
+            
+            currentlyInfected : result = reportedCases * 10;
+            
+        break;
+            
+            severeImpact.currentlyInfected : result = reportedCases * 50;
+            
+        break;
+            
+            infectionsByRequestedTime : result = currentlyInfected * y; 
+            
+        break;
+
+    }
